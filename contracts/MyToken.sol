427ebdc862f5a37a8be5a3b27115cbec294cb177
im1ex <lex.lar@gmail.com>
2025-12-15 21:08:18 +0100

    Updated OpenZeppelin dependency to v5.5.0.

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.0.0
-pragma solidity ^0.8.22;
+// Compatible with OpenZeppelin Contracts ^5.5.0
+pragma solidity ^0.8.27;
 
 import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import {ERC20PausableUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PausableUpgradeable.sol";
-import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {Initializable} from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
 
 contract MyToken is Initializable, ERC20Upgradeable, ERC20PausableUpgradeable, OwnableUpgradeable, UUPSUpgradeable {
     /// @custom:oz-upgrades-unsafe-allow constructor
@@ -18,7 +18,6 @@ contract MyToken is Initializable, ERC20Upgradeable, ERC20PausableUpgradeable, O
         __ERC20_init("MyToken", "MTK");
         __ERC20Pausable_init();
         __Ownable_init(initialOwner);
-        __UUPSUpgradeable_init();
     }
 
     function pause() public onlyOwner {
@@ -29,12 +28,13 @@ contract MyToken is Initializable, ERC20Upgradeable, ERC20PausableUpgradeable, O
         _unpause();
     }
 
-    function mint(uint256 amount) public onlyOwner {
-        _mint(_msgSender(), amount);
+    function mint(address account, uint256 amount) public onlyOwner {
+        _mint(account, amount);
     }
 
-    function burn(uint256 value) public onlyOwner {
-        _burn(_msgSender(), value);
+    function burn(address account, uint256 value) public onlyOwner {
+        _spendAllowance(account, _msgSender(), value);
+        _burn(account, value);
     }
 
     function _authorizeUpgrade(address newImplementation)
