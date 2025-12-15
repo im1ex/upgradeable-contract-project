3e8026651ee4fd92350a0a3871d25a49e808f247
im1ex <lex.lar@gmail.com>
2025-12-15 21:25:29 +0100

    Fixed warnings.

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -20,7 +20,7 @@ contract Lock {
         owner = payable(msg.sender);
     }
 
-    function withdraw() public {
+    function withdraw() public returns (bool success) {
         // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
         // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
 
@@ -29,6 +29,6 @@ contract Lock {
 
         emit Withdrawal(address(this).balance, block.timestamp);
 
-        owner.transfer(address(this).balance);
+        (success, ) = owner.call{value: address(this).balance}("");
     }
 }
